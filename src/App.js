7e789af83e6671cb326cdcd0832c06c1f3e0e708
manojkmishra <manojmishra571980@gmail.com>
2019-12-06 21:02:16 +1100

    both reducers accessed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import {useSelector} from 'react-redux';
 
 function App() {
   const counter=useSelector(state=>state.counter);
+  const islogedin=useSelector(state=>state.isLogged);
   return (
     <div className="App">
 <h1>Counter {counter}</h1>
+{islogedin ? <h2>loggedin</h2> : <h2>not loggedin</h2>}
+
     </div>
   );
 }
