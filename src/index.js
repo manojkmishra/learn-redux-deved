ca248c6c3b9af756d7fe01ca507790a43c5383ed
manojkmishra <manojmishra571980@gmail.com>
2019-12-06 12:51:21 +1100

    redux working

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {createStore} from 'redux';
+//ACTION INCREMENT
+const increment=()=>{ console.log('action-increment');  return { type: 'INCREMENT'}  }//type or name or anything
+const decrement=()=>{ console.log('action-decrement'); return { type: 'DECREMENT'}  }//type or name or anything
+//REDUCER
+const reducercounter=(state=0,action)=>
+    {    switch(action.type)
+            {  case "INCREMENT": {console.log('reducer-case INCREMENT- state',state,'+1');return state + 1;}
+               case "DECREMENT": {console.log('reducer-case DECREMENT- state',state,'-1');return state - 1;}
+            }
+    }
+//STORE
+let store = createStore(reducercounter);
+store.subscribe(()=>console.log(store.getState())) //show console log
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
