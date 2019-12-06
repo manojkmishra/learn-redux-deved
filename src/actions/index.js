fd1b1a4bc86902a44db1112fa93a48013e2416ab
manojkmishra <manojmishra571980@gmail.com>
2019-12-06 21:31:19 +1100

    actons- increment by 5 working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 //ACTION INCREMENT
-export const increment=()=>{ console.log('action-increment');  return { type: 'INCREMENT'}  }//type or name or anything
+export const increment=(x)=>
+{ console.log('action-increment');  
+  return { type: 'INCREMENT', //type is just a name, it can be anything like xyz
+           payload:  x,  //payload is just a name, it can be anything like xyz
+          }  
+}//type or name or anything
 export const decrement=()=>{ console.log('action-decrement'); return { type: 'DECREMENT'}  }//type or name or anything
 //DISPATCH
 
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,6 +1,10 @@
 const counterReducer=(state=0,action)=>
     {    switch(action.type)
-            {  case "INCREMENT": {console.log('counterReducer-case INCREMENT- state',state,'+1');return state + 1;}
+            {  case "INCREMENT": 
+                  {console.log('counterReducer-case INCREMENT- state',state,'+1');
+                    //return state + 1;}
+                    return state + action.payload;
+                  }
                case "DECREMENT": {console.log('counterReducer-case DECREMENT- state',state,'-1');return state - 1;}
                default: return state;
             }
