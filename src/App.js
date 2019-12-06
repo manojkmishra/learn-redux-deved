3329a0d46cedd1e107cf31c92d23078dce4b05ef
manojkmishra <manojmishra571980@gmail.com>
2019-12-06 13:51:14 +1100

    provider and useSelector added to access reducer in a component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import {useSelector} from 'react-redux';
 
 function App() {
+  const counter=useSelector(state=>state.counter);
   return (
     <div className="App">
-     <h1>Hello</h1>
+<h1>Counter {counter}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import allReducer from './reducers'
+import {Provider} from 'react-redux';
 //STORE
 let store = createStore(allReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 store.subscribe(()=>console.log(store.getState())) //show console log
 
-//ACTION INCREMENT
-//const increment=()=>{ console.log('action-increment');  return { type: 'INCREMENT'}  }//type or name or anything
-//const decrement=()=>{ console.log('action-decrement'); return { type: 'DECREMENT'}  }//type or name or anything
-//DISPATCH
-//store.dispatch(increment());
-//store.dispatch(decrement());
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( 
+            <Provider store={store}>
+                <App />
+             </Provider>,
+ document.getElementById('root'));
 serviceWorker.unregister();
